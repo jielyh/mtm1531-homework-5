cf38b1065aba2456c962e2452299e4db1ebbaf89
jielyh <ho000043@algonquinlive.ca>
2012-03-08 17:40:40 -0500

    submitting

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'includes/filter-wrapper.php';
+require_once 'includes/db.php';
+
+// `->exec()` allows us to perform SQL and NOT expect results
+// `->query()` allows us to perform SQL and expect results
+
+//creating 'results' variable ($)
+$results = $db->query(' 
+	SELECT id, movie_title, release_date, director
+	FROM movies
+	ORDER BY movie_title ASC
+');
+
+?><!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Movie!</title>
+</head>
+<body>		
+	<ul>
+	
+	<?php foreach ($results as $movie) : ?>
+		<li>
+			<a href="single.php?id=<?php echo $movie['id']; ?>"><?php echo $movie['movie_title']; ?></a>
+			&bull;
+			<a href="delete.php?id=<?php echo $movie['id']; ?>">Delete</a>
+		</li>
+	<?php endforeach; ?>
+	</ul>
+	
+</body>
+</html>
